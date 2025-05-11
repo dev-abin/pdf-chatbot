5bfbccf3c4f5830ed246354e87b545215e95cea5
dev-abin <abindevmahe@gmail.com>
2025-05-11 21:04:27 +0530

    distilbert as an embedding function and other improvements

--- chatapp.py
+++ chatapp.py
@@ -8,7 +8,8 @@ from langchain.prompts import PromptTemplate
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain.chains import ConversationalRetrievalChain
 from langchain_ollama import OllamaLLM
-from utils import ChatRequest, prompt_template, OLLAMA_API_URL,  PREF_MODEL, get_wikipedia_agent, log_interaction
+from utils import ChatRequest, prompt_template, OLLAMA_API_URL,  PREF_MODEL, PREF_EMBEDDING_MODEL,\
+    initiate_wikipedia_agent, log_interaction
 from logger import logger
 
 app = FastAPI()
@@ -24,9 +25,20 @@ os.makedirs(PDF_DIR, exist_ok=True)
 os.makedirs(VECTOR_DIR, exist_ok=True)
 
 
+# === Upload Endpoint ===
 @app.post("/upload-pdf/")
 async def upload_pdf(file: UploadFile = File(...)):
-    """Upload and process the PDF document if not already processed."""
+    """Upload and process a PDF document if not already processed.
+    
+    Args:
+        file (UploadFile): A required PDF file to be uploaded.
+
+    Returns:
+        dict: A dictionary containing the filename and a success message.
+
+    Raises:
+        HTTPException: If the uploaded file is not a PDF.
+    """
     try:
 
         logger.info(f"Upload PDF endpoint called | endpoint=/upload-pdf/ | uploaded_pdf='{file.filename}'")
@@ -36,8 +48,8 @@ async def upload_pdf(file: UploadFile = File(...)):
             # Return error if file is not a PDF
             raise HTTPException(status_code=400, detail="File must be a PDF")
         
-        content = await file.read() # Read the file content asynchronously
-        file_hash = hashlib.sha256(content).hexdigest() # Generate a unique hash for the file
+        content = await file.read()                          # Read the file content asynchronously
+        file_hash = hashlib.sha256(content).hexdigest()      # Generate a unique hash for the file
         pdf_path = os.path.join(PDF_DIR, f"{file_hash}.pdf") # Define the path to save the file
         
         # Check if the PDF has already been processed
@@ -45,12 +57,15 @@ async def upload_pdf(file: UploadFile = File(...)):
             logger.info(f"{file.filename} has already been processed.")
             return {"message": f"{file.filename} has already been processed."} # Return a message without re-processing
         
+        logger.info("storing the pdf contents to vector store")
+
         # Save the PDF file to the specified directory
         with open(pdf_path, 'wb') as pdf_file:
             pdf_file.write(content)
 
+        # define the embedding_fn
         embedding_fn = HuggingFaceEmbeddings(
-                model_name="sentence-transformers/all-MiniLM-L6-v2"
+                model_name=PREF_EMBEDDING_MODEL
             )
                     
         # Initialize the vector store with embeddings
@@ -76,9 +91,27 @@ async def upload_pdf(file: UploadFile = File(...)):
         raise HTTPException(status_code=500, detail=f"Internal server error: {str(e)}")
 
 
-# === Main Chat Endpoint ===
+# === Chat Endpoint ===
 @app.post("/chat/")
 async def chat(chat_request: ChatRequest):
+    """Handle user chat queries by retrieving relevant information from a vector store or falling back to a Wikipedia agent.
+    
+    This endpoint processes a user's question by querying a preloaded vector store containing document embeddings.
+    If the vector store is empty or unavailable, it raises an error. If the query cannot be answered using the vector store,
+    it falls back to a Wikipedia agent. The response includes the generated answer and sources (either document pages or Wikipedia).
+
+    Args:
+        chat_request (ChatRequest): A request object containing the user's question and chat history.
+
+    Returns:
+        dict: A dictionary containing:
+            - answer (str): The generated answer to the user's question.
+            - sources (list): A list of sources used to generate the answer (e.g., document page numbers or "Wikipedia").
+
+    Raises:
+        HTTPException:
+            - 404: If the vector store directory is not found or empty (indicating no PDF has been uploaded).
+    """
     try:
 
         logger.info(f"Chat endpoint called. | endpoint=/chat/ | query='{chat_request.question}' | chat_history= '{chat_request.chat_history}'")
@@ -87,8 +120,9 @@ async def chat(chat_request: ChatRequest):
         if not os.path.exists(VECTOR_DIR) or not os.listdir(VECTOR_DIR):
             raise HTTPException(status_code=404, detail="Vectorstore not found or empty. Please upload a PDF first.")
         
+        # define the embedding_fn
         embedding_fn = HuggingFaceEmbeddings(
-                model_name="sentence-transformers/all-MiniLM-L6-v2"
+                model_name=PREF_EMBEDDING_MODEL
             )
         
         # Load the vector store with the preprocessed document embeddings
@@ -98,15 +132,14 @@ async def chat(chat_request: ChatRequest):
         prompt = PromptTemplate(
             input_variables=["context", "question"],
             template=prompt_template
-
         )
     
         # Create a conversational retrieval chain
         qa_chain = ConversationalRetrievalChain.from_llm(
             llm=OllamaLLM(model=PREF_MODEL, base_url= OLLAMA_API_URL),    # Load the LLM model
-            retriever=vectorstore.as_retriever(search_kwargs={"k": 3}),     # Retrieve top 3 relevant documents
-            return_source_documents=True,    # Include source documents in the response
-            combine_docs_chain_kwargs={"prompt": prompt},  # apply custom prompt here
+            retriever=vectorstore.as_retriever(search_kwargs={"k": 5}),   # Retrieve top 3 relevant documents
+            return_source_documents=True,                                 # Include source documents in the response
+            combine_docs_chain_kwargs={"prompt": prompt},                 # apply custom prompt here
             verbose = True
         )
         
@@ -115,15 +148,15 @@ async def chat(chat_request: ChatRequest):
 
         # Extract the generated answer from the response
         answer = result.get("answer", "No answer found").strip()
-
-        # logger.info(answer)
+        logger.info(answer)
 
         if "No answer found" in answer:
-
             logger.info(f"Falling back to Wikipedia agent | query='{chat_request.question}' | chat_history= '{chat_request.chat_history}'")
 
-            agent = get_wikipedia_agent()
+            agent = initiate_wikipedia_agent()
+            # invoke the agent with the user query
             agent_result = agent.invoke(chat_request.question)
+            # get the answer from wikipedia agent
             agent_answer = agent_result.get("output", "No answer found")
             
             return {"answer": agent_answer, "sources": ["Wikipedia"]}
